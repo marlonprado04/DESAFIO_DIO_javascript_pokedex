c86e4fc035bc0ed37349385c1c2a1513c45ba454
Marlon Prado <marlonprado04@gmail.com>
2023-09-27 21:19:20 -0300

    feat: add html to detail pokemon, but dont add api logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
+const pokemonProfile = document.getElementById("profileContent");
 
 const maxRecords = 151;
 const limit = 10;
@@ -9,9 +10,12 @@ let offset = 0;
 function redirectToDetails(pokemonNumber) {
   // Cria variável com URL de detalhes + pokemon id
   const detailsUrl = `pokemon_profile.html?id=${pokemonNumber}`;
-
   // Redireciona o usuário para a página details.html
   window.location.href = detailsUrl;
+
+  // Chama função para mostrar perfil do pokemon
+  loadPokemonProfile(pokemonNumber);
+  
 }
 
 // Função mostra detalhes de acordo com clique no menu
@@ -27,6 +31,54 @@ function showItems(category) {
   document.getElementById(category).classList.add("active");
 }
 
+// Função carrega detalhes do perfil do pokemon
+function loadPokemonProfile(pokemon) {
+
+  url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+  return `<!-- Seção de informações básicas -->
+          <section class="basic ${pokemon.type}">
+              <h1 class="name">${pokemon.name}</h1>
+              <ol class="types">
+                ${pokemon.types
+                .map((type) => `<li class="type ${type}"> ${type}</li>`)
+                .join("")}
+              </ol>
+
+              <div class="img">
+                  <img src="${pokemon.photo}"
+                      alt="${pokemon.img}">
+              </div>
+          </section>
+
+          <!-- Seção de detalhes -->
+          <section class="details">
+
+              <div class="menu">
+                  <div class="menu_option" onclick="showItems('about')">About</div>
+                  <div class="menu_option" onclick="showItems('base_stats')">Base Stats</div>
+              </div>
+
+              <div class="menu_items" id="about">
+                  <ol>
+                      <li id="species">Species: ${pokemon.species}</li>
+                      <li id="height">Height: ${pokemon.height}</li>
+                      <li id="weight">Weight: ${pokemon.wight}</li>
+                      <li id="abilities">Abilities: ${pokemon.abilities}</li>
+                  </ol>
+              </div>
+
+              <div class="menu_items" id="base_stats">
+                  <ol>
+                      <li id="hp">HP: ${pokemon.hp}</li>
+                      <li id="attack">Attack: ${pokemon.attack}</li>
+                      <li id="defense">Defense: ${pokemon.defense}</li>
+                  </ol>
+              </div>
+    `;
+}
+
+// Função carrega mais pokemons na pokedex
 function loadMoreItems(offset, limit) {
   // Chamando requisição de API e listando pokemon no browse
   pokeApi
@@ -37,7 +89,7 @@ function loadMoreItems(offset, limit) {
       // Depois incrementa lista convertida em string no html via innerHTML
       const newHtml = pokemons
         .map((pokemon) => {
-          return `<li class="pokemon ${pokemon.type}">
+          return `<li class="pokemon ${pokemon.type}" onclick="redirectToDetails(${pokemon.number})">
                   <span class="number">#${pokemon.number}</span>
                   <span class="name">${pokemon.name}</span>
   
